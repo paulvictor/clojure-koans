8f2db675bbe033e79c0d2fe8ab82c52ffb139c12
Paul Victor Raj <paulvictor@gmail.com>
2024-06-13 19:11:27 +0530

    Finished till vectors

--- src/koans/02_strings.clj
+++ src/koans/02_strings.clj
@@ -49,22 +49,22 @@
   (= "hello world" (string/trim "  \nhello world \t \n"))
 
   "You can check if something is a char"
-  (= __ (char? \c))
+  (= true (char? \c))
 
   "But it may not be"
-  (= __ (char? "a"))
+  (= false (char? "a"))
 
   "But chars aren't strings"
-  (= __ (string? \b))
+  (= false (string? \b))
 
   "Strings are strings"
-  (= true (string? __))
+  (= true (string? "foo"))
 
   "Some strings may be blank"
-  (= __ (string/blank? ""))
+  (= true (string/blank? ""))
 
   "Even if at first glance they aren't"
-  (= __ (string/blank? " \n \t  "))
+  (= true (string/blank? " \n \t  "))
 
   "However, most strings aren't blank"
-  (= __ (string/blank? "hello?\nare you out there?")))
+  (= false (string/blank? "hello?\nare you out there?")))
